425609961afef39c98e8abbe5a4859367d5c5776
mattrock12344-collab <mattrock12344@gmail.com>
2025-11-17 17:39:27 -0500

    :D

---
+++ tests.py
@@ -0,0 +1,18 @@
+from functions.write_file import write_file
+from functions.get_file_content import get_file_content
+from functions.get_files_info import get_files_info
+from functions.run_python_file import run_python_file
+import os
+
+print(run_python_file("calculator", "main.py"))
+
+print(run_python_file("calculator", "main.py", ["3 + 5"]))
+
+print(run_python_file("calculator", "tests.py"))
+
+print(run_python_file("calculator", "../main.py"))
+
+print(run_python_file("calculator", "nonexistent.py"))
+
+print(run_python_file("calculator", "lorem.txt"))
+
